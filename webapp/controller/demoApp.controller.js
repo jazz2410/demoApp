1210602c279a16a2c2d8ef6aa260d2ff68684a30
Dhaliwal <tajinder.dhaliwal@accenture.com>
2024-08-14 10:17:13 +0200

    Added smarttable and panel

--- webapp/controller/demoApp.controller.js
+++ webapp/controller/demoApp.controller.js
@@ -7,6 +7,10 @@ function (Controller) {
     return Controller.extend("demoapp.controller.demoApp", {
         onInit: function () {
 
+        },
+        onRowSelect: function(oEvent){
+            const sPath = oEvent.getSource()._aSelectedPaths[0];
+            this.byId('orderDetailsPanel').bindElement(sPath);
         }
     });
 });
