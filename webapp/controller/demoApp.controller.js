103ef60509b85813aafcb69e1bcdcb0a80b9b29a
Dhaliwal <tajinder.dhaliwal@accenture.com>
2024-08-14 10:38:50 +0200

    Added order items table

--- webapp/controller/demoApp.controller.js
+++ webapp/controller/demoApp.controller.js
@@ -11,6 +11,9 @@ function (Controller) {
         onRowSelect: function(oEvent){
             const sPath = oEvent.getSource()._aSelectedPaths[0];
             this.byId('orderDetailsPanel').bindElement(sPath);
+            const itemsPath = sPath + '/Order_Details';
+            this.byId("orderDetailsTable").setTableBindingPath(itemsPath);
+            this.byId("orderDetailsTable").rebindTable(true);
         }
     });
 });
